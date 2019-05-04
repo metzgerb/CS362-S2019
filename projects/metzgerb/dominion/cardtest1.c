7b17c26a3c1fd1911b64ece3efb50463cb7d9beb
metzgerb <40550164+metzgerb@users.noreply.github.com>
2019-05-04 11:45:37 -0500

    Updated Makefile
    removed commented out lines in make file unittestrestults.
    Also removed unused variables in unit tests.

--- projects/metzgerb/dominion/cardtest1.c
+++ projects/metzgerb/dominion/cardtest1.c
@@ -25,7 +25,6 @@ int main()
 	int pass = 0, fail = 0; //keeps track of the number of tests that passed and failed
 	int newCards = 3;
 	int played = 1;
-	int actionChange = 0;
 	int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
 	int seed = 1000;
 	int numPlayers = 2;
